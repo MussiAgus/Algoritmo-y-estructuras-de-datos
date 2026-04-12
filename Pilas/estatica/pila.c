0c660ad58f62d624149695edcb3011568ba9368e
Agus Mussi <agustinmfacultad@gmail.com>
2026-04-12 20:07:10 -0300

    Pila Estatica

--- Pilas/estatica/pila.c
+++ Pilas/estatica/pila.c
@@ -9,3 +9,13 @@ void crearPila(t_pila* pila){
 void eliminarPila(t_pila* pila){
     pila->tope=TAM_MAX;
 }
+
+int ponerEnPila(t_pila *pila, const void *dato, size_t tamDato){
+    if(pila->tope < (tamDato + sizeof(size_t) ) )
+        return SIN_MEM;
+
+    pila->tope -=tamDato;
+    memcpy(pila->pila+pila->tope, dato, tamDato);
+    pila->tope-= sizeof(size_t);
+    memcpy(pila->pila+pila->tope, &tamDato, sizeof(size_t));
+}
